26994fd26d1e67fe6192501594f9afe7b70f9949
David Ronk <david@expectedbehavior.com>
2020-07-07 17:25:44 -0400

    Added hosted document tests - implement .docraptor_key api_key file for testing hosted docs - created async, sync and expire hosted doc tests

--- test/HostedSync.java
+++ test/HostedSync.java
@@ -1,31 +1,37 @@
 import java.io.*;
+import java.lang.*;
 import java.net.*;
+import java.text.*;
+import java.util.*;
 import com.docraptor.*;
 
-public class Async {
+public class HostedSync {
   public static void main(String[] args) throws Exception {
+    String api_key = "YOUR_API_KEY_HERE";
+    try (BufferedReader br = new BufferedReader(new FileReader("../.docraptor_key"))) {
+      api_key = br.readLine().trim();
+    } catch (IOException e) {
+      throw new RuntimeException("Please put a valid (paid plan) api key in the .docraptor_key file when testing this feature.", e);
+    }
+
     DocApi docraptor = new DocApi();
     ApiClient client = docraptor.getApiClient();
-    client.setUsername("YOUR_API_KEY_HERE");
+    client.setUsername(api_key);
     // client.setDebugging(true);
 
     Doc doc = new Doc();
-    doc.setName("java-async.pdf");
+    doc.setName("java-sync.pdf");
     doc.setDocumentType(Doc.DocumentTypeEnum.PDF);
     doc.setDocumentContent("<html><body>Hello from Java</body></html>");
     doc.setTest(true);
 
-    AsyncDoc response = docraptor.createAsyncDoc(doc);
+    Date tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    df.setTimeZone(TimeZone.getTimeZone("UTC"));
+    String tomorrow_str = df.format(tomorrow);
+    doc.setHostedExpiresAt(tomorrow_str);
 
-    DocStatus status_response = null;
-    while(true) {
-      status_response = docraptor.getAsyncDocStatus(response.getStatusId());
-      if (status_response.getStatus().equals("completed")) {
-        break;
-      }
-      Thread.sleep(1000);
-    }
-
+    DocStatus status_response = docraptor.createHostedDoc(doc);
     byte data[] = docraptor.getAsyncDoc(status_response.getDownloadId());
     FileOutputStream out = new FileOutputStream("/tmp/the-file-name.pdf");
     out.write(data);
